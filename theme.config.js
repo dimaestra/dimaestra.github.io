0aff913d01aa97eaab0e8dad1d212c1d5dafd9e9
agamdimaestra <agamdimaestra@gmail.com>
2022-12-22 19:13:02 +0700

    Added projects

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © Agam Dimaestra.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
